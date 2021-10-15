723ca36c8f675fc414775f8022da402dd2d6f059
Gohryt <kosachev_alex@mail.ru>
2021-10-15 20:12:57 +0300

    init

---
+++ linter.py
@@ -0,0 +1,12 @@
+from SublimeLinter.lint import util, Linter, WARNING
+
+
+class Golint(Linter):
+    cmd = 'revive'
+    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
+    tempfile_suffix = 'go'
+    error_stream = util.STREAM_STDOUT
+    default_type = WARNING
+    defaults = {
+        'selector': 'source.go'
+    }
